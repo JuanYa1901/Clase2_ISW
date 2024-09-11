b6db9350221ba89d28b7926b037d25054650ad9f
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-09-10 22:22:37 -0300

    Variables y tipos de datos

---
+++ Ejercicio1.js
@@ -0,0 +1,16 @@
+const nombre = "Juan" // declarara nombre tipo String y que no se pueda modificar
+let edad = 23 // declara valor  la edad con un int y que si se pueda modificar
+const puedoConducir = true // declara un valor booleano y que no se pueda modificar
+
+// imprime por consola contenido de la variable
+console.log(`el contenido de la variable nombre es: ${nombre}`)
+console.log(`el contenido de la variable edad es: ${edad}`)
+console.log(`el contenido de la puedoConducir nombre es: ${puedoConducir}`)
+
+console.log("\n")
+
+
+// imprime por consola tipo de la variable
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof puedoConducir)
