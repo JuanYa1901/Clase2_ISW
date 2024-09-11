008f7a3248daa5f489f55adacff62cd33b5bfede
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-09-10 22:30:06 -0300

    Condicionales

---
+++ Ejercicio4.js
@@ -0,0 +1,93 @@
+const numero1 = 10
+const numero2 = 20
+const numero3 = 30
+
+// 1) Imprimir en consola el número mayor de los tres
+
+if(numero1 > numero2 && numero1 > numero3){
+    console.log(numero1)
+}else if(numero2 > numero1 && numero2 > numero3){
+    console.log(numero2)
+}else{
+    console.log(numero3)
+}
+
+console.log("\n")
+
+// 2) Imprimir en consola el número menor de los tres
+
+if(numero1 < numero2 && numero1 < numero3){
+    console.log(numero1)
+}else if(numero2 < numero1 && numero2 < numero3){
+    console.log(numero2)
+}else{
+    console.log(numero3)
+}
+
+console.log("\n")
+
+// 3) Imprimir en consola si el numero1 es par o impar
+
+if(numero1%2==0){
+    console.log(`numero 1 es par`)
+}
+else{
+    console.log(`numero 1 es impar`)
+}
+
+console.log("\n")
+
+// 4) Imprimir en consola si el numero2 es par o impar
+
+if(numero2%2==0){
+    console.log(`numero2 es par`)
+}
+else{
+    console.log(`numero2 es impar`)
+}
+
+console.log("\n")
+
+// 5) Imprimir en consola si el numero3 es par o impar
+
+if(numero3%2==0){
+    console.log(`numero3 es par`)
+}
+else{
+    console.log(`numero3 es impar`)
+}
+
+console.log("\n")
+
+// 6) Imprimir en consola si el numero1 es múltiplo de 5
+
+if(numero1%5==0){
+    console.log(`numero1 es multiplo de 5`)
+}
+else{
+    console.log(`numero1 no es multiplo de 5`)
+}
+
+console.log("\n")
+
+// 7) Imprimir en consola si el numero2 es múltiplo de 5
+
+if(numero2%5==0){
+    console.log(`numero2 es multiplo de 5`)
+}
+else{
+    console.log(`numero2 no es multiplo de 5`)
+}
+
+console.log("\n")
+
+// 8) Imprimir en consola si el numero3 es múltiplo de 5
+
+if(numero3%5==0){
+    console.log(`numero3 es multiplo de 5`)
+}
+else{
+    console.log(`numero3 no es multiplo de 5`)
+}
+
+console.log("\n")
