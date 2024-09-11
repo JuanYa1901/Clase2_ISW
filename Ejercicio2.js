cdfed67b45c1f68e23e34dc515d7565751f2a607
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-09-10 22:27:35 -0300

    Operadores Aritmeticos

---
+++ Ejercicio2.js
@@ -0,0 +1,14 @@
+
+const numero1 = 20
+const numero2 = 40
+
+// SUMA
+console.log(numero1+numero2)
+// RESTA
+console.log(numero1-numero2)
+// MULTIPLICAR
+console.log(numero1*numero2)
+// DIVIDIR
+console.log(numero1/numero2)
+// MODULO
+console.log(numero1%numero2)
